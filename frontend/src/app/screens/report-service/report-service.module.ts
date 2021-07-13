345d80075a01f0824c584e453991bdbfc055452f
Johan Sebastian Jimenez Castaño <s.jimenez@utp.edu.co>
2021-07-13 15:51:24 -0500

    Initial project back

---
+++ frontend/src/app/screens/report-service/report-service.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReportServiceRoutingModule } from './report-service-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ReportServiceRoutingModule
+  ]
+})
+export class ReportServiceModule { }
