1ed726c8455ea23271b46f5ab8400ef834a392eb
Johan Sebastian Jimenez Castaño <s.jimenez@utp.edu.co>
2021-07-14 14:49:28 -0500

    Adding function to calculate technician hours

--- backend/src/main/java/com/ias/project/backend/service/implement/CalculateTimeServiceImp.java
+++ backend/src/main/java/com/ias/project/backend/service/implement/CalculateTimeServiceImp.java
@@ -9,7 +9,9 @@ import com.ias.project.backend.model.Report;
 import com.ias.project.backend.repository.ReportRepository;
 import com.ias.project.backend.service.CalculateTimeService;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Objects;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,18 +27,56 @@ public class CalculateTimeServiceImp implements CalculateTimeService {
 
     public Long hoursOfOperation(String idTechnician, Integer weekNumber) {
         List<Report> listReports = reportRepository.findAll();
-        List<Report> reportsByTechnician = getTechnicianById(listReports, 
+        List<Report> reportsByTechnician = getTechnicianById(listReports,
                 idTechnician);
+        List<Report> reportsByTechnicianAndWeekNumber =
+                getTechnicianByWeekNumber(reportsByTechnician, weekNumber);
+        List<Long> hoursOfOperation =
+                getTotalHoursOfOperation(reportsByTechnicianAndWeekNumber);
+        Long totalHoursOfOperation = 0L;
+        totalHoursOfOperation = hoursOfOperation.stream().map((hours) -> hours)
+                .reduce(totalHoursOfOperation, (accumulator, _item) -> 
+                        accumulator + _item);
+        return totalHoursOfOperation;
     }
-    
+
+    private List<Long> getTotalHoursOfOperation(
+            List<Report> reportsByTechnicianAndWeekNumber) {
+        List<Long> totalHoursOfOperation = new ArrayList<>();
+        reportsByTechnicianAndWeekNumber.forEach((report) -> {
+            Long initialTime = report.getInitialDate().getTime();
+            Long finalTime = report.getFinalDate().getTime();
+            Long mili = finalTime - initialTime;
+            Long min = mili/(1000*60);
+            Long hour = min/60;
+            totalHoursOfOperation.add(hour);
+        });
+        return totalHoursOfOperation;
+    }
+
+    private List<Report> getTechnicianByWeekNumber(
+            List<Report> reportsByTechnician, Integer weekNumber) {
+        List<Report> reportsByTechnicianAndWeekNumber = new ArrayList<>();
+        Integer numberWeekOfYear;
+        for (Report report : reportsByTechnician) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(report.getInitialDate());
+            numberWeekOfYear = calendar.get(Calendar.WEEK_OF_YEAR);
+            if (Objects.equals(weekNumber, numberWeekOfYear)) {
+                reportsByTechnicianAndWeekNumber.add(report);
+            }
+        }
+        return reportsByTechnicianAndWeekNumber;
+    }
+
     private List<Report> getTechnicianById(List<Report> listReports,
             String idTechnician) {
         List<Report> reportsByTechnician = new ArrayList<>();
-        listReports.stream().filter((report) -> 
-                (report.getIdTechnician().equals(idTechnician)))
+        listReports.stream().filter((report)
+                -> (report.getIdTechnician().equals(idTechnician)))
                 .forEachOrdered((report) -> {
-            reportsByTechnician.add(report);
-        });
+                    reportsByTechnician.add(report);
+                });
         return reportsByTechnician;
     }
 }
