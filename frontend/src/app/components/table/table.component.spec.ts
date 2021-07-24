2b6b2559338c6756c2432a46ca49bfac18baaea9
Johan Sebastian Jimenez <sebasjimenez9944@gmail.com>
2021-07-23 23:03:27 -0500

    Modify table calendar and add modules to testing file

--- frontend/src/app/components/table/table.component.spec.ts
+++ frontend/src/app/components/table/table.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { TableComponent } from './table.component';
 
@@ -8,7 +10,11 @@ describe('TableComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TableComponent ]
+      declarations: [ TableComponent ],
+      imports: [
+        ReactiveFormsModule,
+        HttpClientTestingModule
+      ]
     })
     .compileComponents();
   });
